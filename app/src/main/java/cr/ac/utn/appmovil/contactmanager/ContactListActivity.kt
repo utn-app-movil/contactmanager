f53e8ad903862e9b4035864a897a549a6a69615a
Ever Barahona <ebarahona@utn.ac.cr>
2021-09-29 20:19:49 -0600

    menu behavior

--- app/src/main/java/cr/ac/utn/appmovil/contactmanager/ContactListActivity.kt
+++ app/src/main/java/cr/ac/utn/appmovil/contactmanager/ContactListActivity.kt
@@ -1,18 +1,11 @@
 package cr.ac.utn.appmovil.contactmanager
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import cr.ac.utn.appmovil.model.*
 
-class ContactActivity : AppCompatActivity() {
-
+class ContactListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact)
-
-
+        setContentView(R.layout.activity_contact_list)
     }
 }
