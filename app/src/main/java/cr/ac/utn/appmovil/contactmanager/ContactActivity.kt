441fceca0e39661c509704cccce5c5bae4688864
Ever Iván Barahona Mendoza <ebarahona@utn.ac.cr>
2021-10-12 00:15:28 +0000

    Semana4

--- app/src/main/java/cr/ac/utn/appmovil/contactmanager/ContactActivity.kt
+++ app/src/main/java/cr/ac/utn/appmovil/contactmanager/ContactActivity.kt
@@ -1,22 +1,37 @@
 package cr.ac.utn.appmovil.contactmanager
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
-import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import cr.ac.utn.appmovil.model.*
+import cr.ac.utn.appmovil.identities.Contact
+import cr.ac.utn.appmovil.model.ContactModel
+import cr.ac.utn.appmovil.util.EXTRA_MESSAGE_CONTACTID
+import java.lang.Exception
 
 class ContactActivity : AppCompatActivity() {
+
+    lateinit var txtName: EditText
+    lateinit var txtLastName: EditText
+    lateinit var txtPhone: EditText
+    lateinit var txtEmail: EditText
+    lateinit var txtAddress: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
+        txtName = findViewById<EditText>(R.id.txtContactName)
+        txtLastName = findViewById<EditText>(R.id.txtContactLastName)
+        txtPhone = findViewById<EditText>(R.id.txtContactPhone)
+        txtEmail = findViewById<EditText>(R.id.txtContactEmail)
+        txtAddress = findViewById<EditText>(R.id.txtContactAddress)
+
+        val contactId = intent.getStringExtra(EXTRA_MESSAGE_CONTACTID)
+        if (contactId != null && contactId != "") loadEditContact(contactId.toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,11 +46,68 @@ class ContactActivity : AppCompatActivity() {
                 saveContact()
                 true
             }
+            R.id.mnuDelete -> {
+                deleteContact()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
     fun saveContact(){
-        Toast.makeText(this, getString(R.string.msgSuccess).toString(),Toast.LENGTH_LONG).show()
+        try {
+            val contact = Contact()
+            contact.Name = txtName.text.toString()
+            contact.LastName = txtLastName.text.toString()
+            contact.Phone = txtPhone.text.toString()?.toInt()
+            contact.Email = txtEmail.text.toString()
+            contact.Address = txtAddress.text.toString()
+
+            if (dataValidation(contact)){
+                ContactModel.addContact(contact)
+                cleanScreen()
+                Toast.makeText(this, getString(R.string.msgSave).toString(),Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, getString(R.string.msgInvalidData).toString(),Toast.LENGTH_LONG).show()
+            }
+        }catch (e: Exception){
+            Toast.makeText(this, e.message.toString(),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun deleteContact(){
+        //if (dataValidation()){
+
+            Toast.makeText(this, getString(R.string.msgDelete).toString(),Toast.LENGTH_LONG).show()
+        //}else{
+          //  Toast.makeText(this, getString(R.string.msgInvalidData).toString(),Toast.LENGTH_LONG).show()
+        //}
+    }
+
+    fun dataValidation(contact: Contact): Boolean{
+        return contact.Name.length > 0 && contact.LastName.length > 0 && contact.Address.length > 0 && contact.Email.length > 0 && contact.Phone > 0
+    }
+
+    fun cleanScreen(){
+        txtName.setText("")
+        txtLastName.setText("")
+        txtPhone.setText("")
+        txtEmail.setText("")
+        txtAddress.setText("")
+    }
+
+    fun loadEditContact(id: String){
+        try{
+            val contact = ContactModel.getContact(id)
+            txtName.setText(contact.Name)
+            txtLastName.setText(contact.LastName)
+            txtPhone.setText(contact.Phone.toString())
+            txtEmail.setText(contact.Email)
+            txtAddress.setText(contact.Address)
+        }catch (e: Exception){
+            Toast.makeText(this, e.message.toString(),Toast.LENGTH_LONG).show()
+        }
+        //val btnDelete = findViewById<MenuItem>(R.id.mnuDelete)
+        //btnDelete.setVisible(false)
     }
 }
--- app/src/main/java/cr/ac/utn/appmovil/util/util.kt
+++ app/src/main/java/cr/ac/utn/appmovil/util/util.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.content.ContextCompat.startActivity
 
+const val EXTRA_MESSAGE_CONTACTID = "cr.ac.utn.appmovil.ContactId"
+
 class util {
     companion object  {
-        fun openActivity(context: Context, objclass: Class<*>){
-            val intent = Intent(context, objclass)
+        fun openActivity(context: Context, objclass: Class<*>, extraName: String, value: String?){
+            val intent = Intent(context, objclass).apply { putExtra(extraName, value)}
             startActivity(context, intent, null)
         }
     }
